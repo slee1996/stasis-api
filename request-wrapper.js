d407029461d980819fef5745f3622ae5f5e2c2b6
slee1996 <spencerlee96@gmail.com>
2021-10-15 11:31:03 -0600

    fixing things

--- request-wrapper.js
+++ request-wrapper.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 module.exports = async (endpoint, apiKey) => {
-  return await axios.get(`https://www.bungie.net/Platform${endpoint}}`,
+  return await axios.get(`https://www.bungie.net/Platform${endpoint}`,
   {
     headers: {'X-API-Key': process.env.BUNGIE_API_KEY}
   })
