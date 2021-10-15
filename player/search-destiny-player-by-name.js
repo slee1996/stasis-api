18bd42e103878957b4e8f03ff3a2d8cf8c965aad
slee1996 <spencerlee96@gmail.com>
2021-10-15 11:35:08 -0600

    changing things

--- player/search-destiny-player-by-name.js
+++ player/search-destiny-player-by-name.js
@@ -1,9 +1,11 @@
 /**
  * Returns a list of Destiny memberships given a global Bungie Display Name. This method will hide overridden memberships due to cross save.
+ * @param {string} membershipType - The type of membership to search for, defaults to All
+ * @param {string} displayName - The full bungie global display name to look up, include the # and the code at the end. This is an exact match lookup.
  */
 
 const requestWrapper = require('../request-wrapper')
 
-module.exports = async (membershipType, displayName) => {
+module.exports = async (membershipType = 'All', displayName) => {
   return await requestWrapper(`/Destiny2/SearchDestinyPlayer/${membershipType}/${displayName}/`)
 }
