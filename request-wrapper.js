ceb5241811254471da4208f9772f4e2b1ae91c38
slee1996 <spencerlee96@gmail.com>
2021-10-17 15:36:02 -0600

    add more functions

--- request-wrapper.js
+++ request-wrapper.js
@@ -1,6 +1,11 @@
 const axios = require('axios')
 
-module.exports = async ({method, url, body}) => {
+module.exports = async ({
+  method, 
+  url, 
+  body, 
+  params
+}) => {
   return await axios({
     baseURL: 'https://www.bungie.net/Platform',
     headers: {'X-API-Key': process.env.BUNGIE_API_KEY},
