d29aaaddfe4ef2262a214db4fba479a228128beb
slee1996 <spencerlee96@gmail.com>
2022-03-02 20:26:46 -0700

    improve

--- player/get-membership-id-by-name.js
+++ player/get-membership-id-by-name.js
@@ -2,6 +2,6 @@ const searchDestinyPlayerByName = require('./search-destiny-player-by-name')
 
 module.exports = async ({displayName}) => {
   const response = await searchDestinyPlayerByName({displayName})
-  console.log(response)
+  console.log(response.data)
   return response.data.Response[0].membershipId
 }
